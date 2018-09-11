b3cb67023fb094e4e7e8d4efb2ac7589a2db2979
Zachkery <zachkery999@gmail.com>
2018-09-11 12:02:29 -0500

    Flash/Validations
    Added simple validations to check the the information coming in from request.POST.  If any issues, display the errors created through flash messages on index.html.
    No issues. Preceding to place validated information into the sqllite db

--- apps/logreg/models.py
+++ apps/logreg/models.py
@@ -2,13 +2,39 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from datetime import date, datetime
+today = date.today()
+import re
+EMAIL_REGEX = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
 # Create your models here.
+
+class UserManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['first_name']) < 2:
+            errors['first_name'] = 'First Name must be at least 2 characters'
+        if len(postData['last_name']) < 2:
+            errors['last_name'] = 'Last Name must be at least 2 characters'
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = 'Email is not Valid'
+        if postData['dob']:
+            if datetime.strptime(postData['dob'], '%Y-%m-%d') > datetime.strptime(str(today), '%Y-%m-%d'):
+                errors['dob'] = 'Date of Birth is not Valid'
+        else:
+            errors['dob'] = 'Please select a Date of Birth'
+        if len(postData['password']) < 8:
+            errors['password'] = 'Password must be at least 8 characters'
+        if postData['password'] != postData['confirm_password']:
+            errors['confirm_password'] = 'Password does not match Confirm Password'
+        return errors
 
 class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
+    email = models.CharField(max_length=255)
+    date_of_birth = models.CharField(max_length=12)
     password = models.CharField(max_length=255)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
+
+    objects = UserManager()
--- apps/logreg/views.py
+++ apps/logreg/views.py
@@ -1,22 +1,54 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, HttpResponse
+from .models import User
+from django.contrib import messages
+from datetime import date, datetime
+import bcrypt
+today = date.today()
 # Create your views here.
 
 # Registration/Login Page
+
+
 def index(request):
     return render(request, 'logreg/index.html')
 
 # Registration/Login Confirmation Page
+
+
 def success(request):
     return render(request, 'logreg/success.html')
 
 # Process Registration
+
+
 def regist(request):
-    return redirect('/success')
+    errors = User.objects.basic_validator(request.POST)
+    # print len(errors)
+    # print errors
+    hashed_pw = bcrypt.hashpw(
+        request.POST['password'].encode(), bcrypt.gensalt())
+    if len(errors):
+        for tag, error in errors.iteritems():
+            messages.error(request, error, extra_tags=tag)
+        # print request.POST['dob']
+        # print today
+        return redirect('/')
+    else:
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        dob = request.POST['dob']
+        User.objects.create(first_name=first_name, last_name=last_name,
+                            email=email, date_of_birth=dob, password=hashed_pw)
+        
+
+        return redirect('/success')
 
 # Verification of login
+
+
 def login(request):
+    # print bcrypt.checkpw(str(request.POST['password']), hashed_pw.encode())
     return redirect('/success')
